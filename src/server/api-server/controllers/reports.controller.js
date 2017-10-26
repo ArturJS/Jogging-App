e2bf7124bf36737ca8c1c482845c8b9c17f4562f
ArturN <nizamutdin.art@yandex.ru>
2017-10-26 17:24:04 +0400

    Fixed wrong calculation of week in reports (in some edge cases)

--- src/server/api-server/controllers/reports.controller.js
+++ src/server/api-server/controllers/reports.controller.js
@@ -14,16 +14,27 @@ export const reportsController = {
 // private methods
 
 function _mapRecordsToReports(recordsList) {
-  let groupedByWeekRecords = _.groupBy(recordsList, (record) => {
-    return moment(+record.date).week();
-  });
-  const minWeek = _.min(Object.keys(groupedByWeekRecords));
-  groupedByWeekRecords = _.mapKeys(groupedByWeekRecords, (value, key) => key - minWeek + 1);
+  const sortedByDateRecords = _.sortBy(recordsList, 'date');
+  const firstRecord = sortedByDateRecords[0];
+
+  firstRecord.week = 1;
+  const firstRecordDate = moment(+firstRecord.date);
+
+  for (let i = 1; i < sortedByDateRecords.length; i++) {
+    const recordDate = moment(+sortedByDateRecords[i].date);
+    const diffInWeeks = Math.ceil(
+      recordDate.diff(firstRecordDate, 'days') / 7
+    );
+    const oneLessWeek = +(diffInWeeks === 1 && recordDate.week() === firstRecordDate.week());
+
+    sortedByDateRecords[i].week = diffInWeeks - oneLessWeek + 1;
+  }
+
+  const groupedByWeekRecords = _.groupBy(sortedByDateRecords, 'week');
 
   const reportsList = _.map(groupedByWeekRecords, (records, key) => {
     const averageDistance = _.sumBy(records, 'distance') / records.length;
     const averageSpeed = _.sumBy(records, 'averageSpeed') / records.length;
-
     return {
       week: key,
       averageDistance,
@@ -31,5 +42,5 @@ function _mapRecordsToReports(recordsList) {
     };
   });
 
-  return _.sortBy(reportsList, 'week');
+  return reportsList;
 }
