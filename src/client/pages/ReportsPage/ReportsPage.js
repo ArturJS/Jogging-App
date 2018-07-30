43a6f0ccf1463c836a5a23e9b9314c9e09bdcc4e
ArturJS <nizamutdin.art@gmail.com>
2018-07-30 06:57:32 +0400

    Connected /reports with graphql api

--- src/client/pages/ReportsPage/ReportsPage.js
+++ src/client/pages/ReportsPage/ReportsPage.js
@@ -1,17 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import ReactTable from 'react-table';
-
-import {reportsApi} from '../../common/api/reportsApi';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import './ReportsPage.scss';
 
+const REPORTS_QUERY = gql`
+  query {
+    reports {
+      week
+      averageSpeed
+      averageDistance
+    }
+  }
+`;
+
+@graphql(REPORTS_QUERY, {
+  name: 'reportsQuery'
+})
 @inject('userStore')
 @observer
 export default class ReportsPage extends Component {
   static propTypes = {
-    userStore: PropTypes.object.isRequired
+    userStore: PropTypes.object.isRequired,
+    reportsQuery: PropTypes.object.isRequired
   };
 
   state = {
@@ -54,39 +68,45 @@ export default class ReportsPage extends Component {
     });
   }
 
-  async componentDidMount() {
-    const reportsList = await reportsApi.getAllReports({showLoading: true});
-    this.setState(({reportsGrid}) => ({
+  componentWillReceiveProps(nextProps) {
+    const { reports } = nextProps.reportsQuery;
+
+    this.updateReports(reports);
+  }
+
+  updateReports(reports) {
+    this.setState(({ reportsGrid }) => ({
       reportsGrid: {
-        data: reportsList,
+        data: reports,
         columns: reportsGrid.columns
       }
     }));
   }
 
   render() {
-    const {reportsGrid} = this.state;
+    const { reportsGrid } = this.state;
 
     return (
       <div className="page reports-page">
-        <Helmet title="Reports"/>
+        <Helmet title="Reports" />
         <h1>Reports</h1>
 
-        {reportsGrid.data.length === 0 &&
-        <div className="no-reports-placeholder">
-          <p>There are no records to display reports...</p>
-        </div>
-        }
+        {reportsGrid.data.length === 0 && (
+          <div className="no-reports-placeholder">
+            <p>There are no records to display reports...</p>
+          </div>
+        )}
 
-        {reportsGrid.data.length > 0 &&
-        <ReactTable
-          className={'reports-table'}
-          data={reportsGrid.data}
-          columns={reportsGrid.columns}
-          pageSize={reportsGrid.data.length}
-          showPageSizeOptions={false}
-          showPagination={false}/>
-        }
+        {reportsGrid.data.length > 0 && (
+          <ReactTable
+            className={'reports-table'}
+            data={reportsGrid.data}
+            columns={reportsGrid.columns}
+            pageSize={reportsGrid.data.length}
+            showPageSizeOptions={false}
+            showPagination={false}
+          />
+        )}
       </div>
     );
   }
