b11eab3a41d52465ab123dbc3211bf04907a5584
ArturJS <nizamutdin.art@gmail.com>
2018-09-17 22:04:52 +0400

    Fix wrong placement for auth mutations

--- src/server/api-server/graphql-components/auth/mutations.js
+++ src/server/api-server/graphql-components/auth/mutations.js
@@ -1,8 +1,68 @@
-import {UserError} from 'graphql-errors';
+import { UserError } from 'graphql-errors';
+import { SignUpType, UserType } from './schema';
+import { GraphQLBoolean } from 'graphql';
+import { UserError } from 'graphql-errors';
+import { authUtils } from '../../utils';
 import db from '../../models';
-import {SignUpType, UserType} from './schema';
 
-export const SignUp = {
+export const signIn = {
+  type: UserType,
+  description: 'Sign In',
+  args: {
+    signIn: {
+      name: 'Sign In Object',
+      type: SignInType
+    }
+  },
+  resolve: (root, args, { req }) => {
+    const { signIn } = args;
+
+    return new Promise((resolve, reject) => {
+      db.User.find({
+        where: {
+          email: signIn.email
+        }
+      }).then(user => {
+        if (!user) {
+          reject(new UserError('Wrong email or password'));
+
+          return;
+        }
+
+        db.User.validPassword(
+          signIn.password,
+          user.password,
+          (error, user) => {
+            if (error) {
+              reject(new UserError(error));
+
+              return;
+            }
+
+            resolve(user);
+          },
+          user
+        );
+
+        req.login(user, {}, err => {
+          if (err) {
+            reject(new UserError(`Sign In failed. ${err}`));
+          }
+        });
+      });
+    });
+  }
+};
+
+export const signOut = {
+  type: GraphQLBoolean,
+  description: 'Sign Out',
+  resolve: async (root, args, { req, res }) => {
+    authUtils.destroySession(req, res);
+  }
+};
+
+export const signUp = {
   type: UserType,
   description: 'Sign Up',
   args: {
@@ -11,15 +71,10 @@ export const SignUp = {
       type: SignUpType
     }
   },
-  resolve: async (root, args, {req, res}) => {
+  resolve: async (root, args, { req, res }) => {
     await _validateSignUp(args.signUp);
 
-    const {
-      firstName,
-      lastName,
-      email,
-      password
-    } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
     const user = {
       firstName,
@@ -36,7 +91,7 @@ export const SignUp = {
       throw new UserError(`Sign Up failed. ${err}`);
     }
 
-    req.login(user, {}, (err) => {
+    req.login(user, {}, err => {
       if (err) {
         throw new UserError(`Sign In failed. ${err}`);
       }
@@ -54,10 +109,7 @@ async function _validateSignUp(signUpPayload) {
 }
 
 async function _passwordValidator(signUpPayload) {
-  const {
-    password,
-    repeatPassword
-  } = signUpPayload;
+  const { password, repeatPassword } = signUpPayload;
 
   if (password !== repeatPassword) {
     throw new UserError('Password and repeat password do not match!');
@@ -65,8 +117,8 @@ async function _passwordValidator(signUpPayload) {
 }
 
 async function _emailUniquenessValidator(signUpPayload) {
-  const {email} = signUpPayload;
-  const userWithSameEmail = await db.User.find({where: {email}});
+  const { email } = signUpPayload;
+  const userWithSameEmail = await db.User.find({ where: { email } });
 
   if (userWithSameEmail) {
     throw new UserError('User with the same email already exists!');
