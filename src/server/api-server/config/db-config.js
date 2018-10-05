7aa6b6e8697105aad22ae4001e1ea67dc398534e
ArturJS <nizamutdin.art@gmail.com>
2018-10-05 21:22:54 +0400

    Added .env config file support

--- src/server/api-server/config/db-config.js
+++ src/server/api-server/config/db-config.js
@@ -1,46 +1,38 @@
-const devConnectionString = 'postgres://postgres:root@127.0.0.1:5432/jogging-app';
-const {DATABASE_URL = devConnectionString} = process.env;
+require('../../../dotenv-import');
 
-const dbParamsRegExp = new RegExp([
-  '^[^:]+:\/\/',  // scheme
-  '([^:]+):',     // username
-  '([^@]+)@',     // password
-  '([^:]+):',     // host
-  '([^/]+)\\/',   // port
-  '(.+)$'         // database
-].join(''));
-
-const [,
+const { DATABASE_URL } = process.env;
+const dbParamsRegExp = new RegExp(
+  [
+    '^[^:]+://', // scheme
+    '([^:]+):', // username
+    '([^@]+)@', // password
+    '([^:]+):', // host
+    '([^/]+)\\/', // port
+    '(.+)$' // database
+  ].join('')
+);
+const [
+  ,
   DB_USERNAME,
   DB_PASSWORD,
   DB_HOSTNAME,
   DB_PORT,
   DB_NAME
 ] = dbParamsRegExp.exec(DATABASE_URL);
+const dbconfig = {
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  host: DB_HOSTNAME,
+  port: DB_PORT,
+  dialect: 'postgres',
+  ssl: true,
+  dialectOptions: {
+    ssl: true
+  }
+};
 
 module.exports = {
-  development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    port: DB_PORT,
-    dialect: 'postgres',
-    // ssl: true,
-    // dialectOptions: {
-    //   ssl: true
-    // }
-  },
-  production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    port: DB_PORT,
-    dialect: 'postgres',
-    // ssl: true,
-    // dialectOptions: {
-    //   ssl: true
-    // }
-  }
+  development: dbconfig,
+  production: dbconfig
 };
