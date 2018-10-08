b79cf89ffbc01bd52405a3eab927ea7cc87ec84c
ArturJS <nizamutdin.art@gmail.com>
2018-10-08 21:53:17 +0400

    Fixed runtime error in dev environment (related to next-sass and next-css plugins)

--- scripts/tools/nextjs/extensions/with-all-extensions.js
+++ scripts/tools/nextjs/extensions/with-all-extensions.js
@@ -1,15 +1,12 @@
 const _ = require('lodash');
 const withExcludeModules = require('./with-exclude-modules');
 const withFonts = require('./with-fonts');
-const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
+const withStyles = require('./with-styles');
 const withFixGraphql = require('./with-fix-graphql');
 
 module.exports = _.flow([
   withExcludeModules,
   withFonts,
-  // todo fix dev styles https://github.com/zeit/next.js/issues/5152
-  withCss,
-  withSass,
+  withStyles,
   withFixGraphql
 ]);
