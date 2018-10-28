ff8d29b9c04d4d1d5565d6bc36329fcd59c8b533
ArturJS <nizamutdin.art@gmail.com>
2018-10-28 18:59:44 +0400

    Introduced prettier

--- src/dotenv-import.js
+++ src/dotenv-import.js
@@ -1,10 +1,10 @@
 const isHerokuEnvironment =
-  process.env.NODE && ~process.env.NODE.indexOf('heroku');
+    process.env.NODE && process.env.NODE.indexOf('heroku') > -1;
 
 if (!isHerokuEnvironment) {
-  // eslint-disable-next-line global-require
-  require('dotenv-safe').config({
-    example: './.env.example',
-    path: './.env'
-  });
+    // eslint-disable-next-line global-require
+    require('dotenv-safe').config({
+        example: './.env.example',
+        path: './.env'
+    });
 }
