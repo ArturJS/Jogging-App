2999d9b741b07bea6aaa4e5fa6d3b9d0865e78e8
ArturJS <nizamutdin.art@gmail.com>
2018-10-28 18:46:24 +0400

    Introduced pre-commit checks

--- local_modules/next-routes/next-routes.js
+++ local_modules/next-routes/next-routes.js
@@ -6,215 +6,19 @@ import NextRouter from 'next/router';
 import _ from 'lodash';
 import cx from 'classnames';
 
-export const performRedirect = ({ req, res, redirectTo }) => {
-  res.writeHead(301, {
-    Location: `${req.protocol}://${req.headers.host}${redirectTo}`
-  });
-  res.end();
-};
+const toQueryString = obj =>
+  Object.keys(obj)
+    .filter(key => obj[key] !== null && obj[key] !== undefined)
+    .map(key => {
+      let value = obj[key];
 
-class Routes {
-  constructor({ Link = NextLink, Router = NextRouter } = {}) {
-    this.routes = [];
-    this.Link = this.getLink(Link);
-    this.Router = this.getRouter(Router);
-  }
-
-  add({ name, pattern, page, onlyForAuthenticated, onlyForUnauthenticated }) {
-    let options;
-
-    if (name instanceof Object) {
-      options = name;
-      name = options.name;
-    } else {
-      if (name[0] === '/') {
-        page = pattern;
-        pattern = name;
-        name = null;
+      if (Array.isArray(value)) {
+        value = value.join('/');
       }
 
-      options = {
-        name,
-        pattern,
-        page
-      };
-    }
-
-    if (this.findByName(name)) {
-      throw new Error(`Route "${name}" already exists`);
-    }
-
-    _.extend(options, {
-      onlyForAuthenticated,
-      onlyForUnauthenticated
-    });
-
-    this.routes.push(new Route(options));
-
-    return this;
-  }
-
-  findByName(name) {
-    if (name) {
-      return this.routes.filter(route => route.name === name)[0];
-    }
-  }
-
-  match(url) {
-    const parsedUrl = parse(url, true);
-    const { pathname, query } = parsedUrl;
-    const traverseOverRoutes = (result, route) => {
-      if (result.route) {
-        return result;
-      }
-
-      const params = route.match(pathname);
-
-      if (!params) {
-        return result;
-      }
-
-      return _.extend({}, result, {
-        route,
-        params,
-        query: _.extend({}, query, params)
-      });
-    };
-
-    return this.routes.reduce(traverseOverRoutes, {
-      query,
-      parsedUrl
-    });
-  }
-
-  findAndGetUrls(nameOrUrl, params) {
-    const route = this.findByName(nameOrUrl);
-
-    if (route) {
-      return {
-        route,
-        urls: route.getUrls(params),
-        byName: true
-      };
-    } else {
-      const { route, query } = this.match(nameOrUrl);
-      const href = route ? route.getHref(query) : nameOrUrl;
-      const urls = { href, as: nameOrUrl };
-
-      return {
-        route,
-        urls
-      };
-    }
-  }
-
-  getRequestHandler(app, customHandler) {
-    const nextHandler = app.getRequestHandler();
-
-    return (req, res) => {
-      const { route, query, parsedUrl } = this.match(req.url);
-
-      if (route) {
-        if (customHandler) {
-          customHandler({ req, res, route, query });
-        } else {
-          const isAuthenticated = req.isAuthenticated();
-
-          if (route.onlyForUnauthenticated && isAuthenticated) {
-            const { redirectTo } = route.onlyForUnauthenticated;
-            performRedirect({ req, res, redirectTo });
-
-            return;
-          } else if (route.onlyForAuthenticated && !isAuthenticated) {
-            const { redirectTo } = route.onlyForAuthenticated;
-            performRedirect({ req, res, redirectTo });
-
-            return;
-          }
-
-          app.render(req, res, route.page, query);
-        }
-      } else {
-        nextHandler(req, res, parsedUrl);
-      }
-    };
-  }
-
-  getLink(Link) {
-    const LinkRoutes = props => {
-      const {
-        route,
-        params,
-        className,
-        activeClassName,
-        children,
-        ...newProps
-      } = props;
-      const nameOrUrl = route;
-
-      if (nameOrUrl) {
-        Object.assign(newProps, this.findAndGetUrls(nameOrUrl, params).urls);
-      }
-
-      const isActiveRoute =
-        props.route === undefined
-          ? false
-          : this.findAndGetUrls(props.route, props.params).urls.as ===
-            this.Router.asPath;
-
-      return (
-        <Link {...newProps}>
-          <a
-            className={cx(className, {
-              [activeClassName]: isActiveRoute
-            })}
-          >
-            {children}
-          </a>
-        </Link>
-      );
-    };
-
-    return LinkRoutes;
-  }
-
-  getRouter(Router) {
-    const disallowTransition = routePath => {
-      const { route } = this.match(routePath);
-      const { isAuthenticated } = Router;
-
-      return (
-        route &&
-        ((route.onlyForUnauthenticated && isAuthenticated) ||
-          (route.onlyForAuthenticated && !isAuthenticated))
-      );
-    };
-    const wrap = method => (route, params, options) => {
-      const {
-        byName,
-        urls: { as, href }
-      } = this.findAndGetUrls(route, params);
-
-      if (disallowTransition(route)) {
-        return;
-      }
-
-      return Router[method](href, as, byName ? options : params);
-    };
-
-    Router.pushRoute = wrap('push');
-    Router.replaceRoute = wrap('replace');
-    Router.prefetchRoute = wrap('prefetch');
-
-    Router.toggleAuth = isAuthenticated => {
-      Router.isAuthenticated = _.isBoolean(isAuthenticated)
-        ? isAuthenticated
-        : !Router.isAuthenticated;
-    };
-
-    return Router;
-  }
-}
+      return [encodeURIComponent(key), encodeURIComponent(value)].join('=');
+    })
+    .join('&');
 
 class Route {
   constructor({
@@ -244,6 +48,8 @@ class Route {
     if (values) {
       return this.valuesToParams(values.slice(1));
     }
+
+    return null;
   }
 
   valuesToParams(values) {
@@ -293,18 +99,224 @@ class Route {
   }
 }
 
-const toQueryString = obj =>
-  Object.keys(obj)
-    .filter(key => obj[key] !== null && obj[key] !== undefined)
-    .map(key => {
-      let value = obj[key];
+export const performRedirect = ({ req, res, redirectTo }) => {
+  res.writeHead(301, {
+    Location: `${req.protocol}://${req.headers.host}${redirectTo}`
+  });
+  res.end();
+};
 
-      if (Array.isArray(value)) {
-        value = value.join('/');
+class Routes {
+  constructor({ Link = NextLink, Router = NextRouter } = {}) {
+    this.routes = [];
+    this.Link = this.getLink(Link);
+    this.Router = this.getRouter(Router);
+  }
+
+  add({ name, pattern, page, onlyForAuthenticated, onlyForUnauthenticated }) {
+    let options;
+
+    if (name instanceof Object) {
+      options = name;
+      // eslint-disable-next-line prefer-destructuring, no-param-reassign
+      name = options.name;
+    } else {
+      if (name[0] === '/') {
+        /* eslint-disable no-param-reassign */
+        page = pattern;
+        pattern = name;
+        name = null;
+        /* eslint-enable no-param-reassign */
       }
 
-      return [encodeURIComponent(key), encodeURIComponent(value)].join('=');
-    })
-    .join('&');
+      options = {
+        name,
+        pattern,
+        page
+      };
+    }
+
+    if (this.findByName(name)) {
+      throw new Error(`Route "${name}" already exists`);
+    }
+
+    _.extend(options, {
+      onlyForAuthenticated,
+      onlyForUnauthenticated
+    });
+
+    this.routes.push(new Route(options));
+
+    return this;
+  }
+
+  findByName(name) {
+    if (name) {
+      return this.routes.filter(route => route.name === name)[0];
+    }
+
+    return null;
+  }
+
+  match(url) {
+    const parsedUrl = parse(url, true);
+    const { pathname, query } = parsedUrl;
+    const traverseOverRoutes = (result, route) => {
+      if (result.route) {
+        return result;
+      }
+
+      const params = route.match(pathname);
+
+      if (!params) {
+        return result;
+      }
+
+      return _.extend({}, result, {
+        route,
+        params,
+        query: _.extend({}, query, params)
+      });
+    };
+
+    return this.routes.reduce(traverseOverRoutes, {
+      query,
+      parsedUrl
+    });
+  }
+
+  findAndGetUrls(nameOrUrl, params) {
+    const foundRoute = this.findByName(nameOrUrl);
+
+    if (foundRoute) {
+      return {
+        route: foundRoute,
+        urls: foundRoute.getUrls(params),
+        byName: true
+      };
+    }
+    const { route, query } = this.match(nameOrUrl);
+    const href = route ? route.getHref(query) : nameOrUrl;
+    const urls = { href, as: nameOrUrl };
+
+    return {
+      route,
+      urls
+    };
+  }
+
+  getRequestHandler(app, customHandler) {
+    const nextHandler = app.getRequestHandler();
+
+    return (req, res) => {
+      const { route, query, parsedUrl } = this.match(req.url);
+
+      if (route) {
+        if (customHandler) {
+          customHandler({ req, res, route, query });
+        } else {
+          const isAuthenticated = req.isAuthenticated();
+
+          if (route.onlyForUnauthenticated && isAuthenticated) {
+            const { redirectTo } = route.onlyForUnauthenticated;
+            performRedirect({ req, res, redirectTo });
+
+            return;
+          }
+          if (route.onlyForAuthenticated && !isAuthenticated) {
+            const { redirectTo } = route.onlyForAuthenticated;
+            performRedirect({ req, res, redirectTo });
+
+            return;
+          }
+
+          app.render(req, res, route.page, query);
+        }
+      } else {
+        nextHandler(req, res, parsedUrl);
+      }
+    };
+  }
+
+  getLink(Link) {
+    const LinkRoutes = props => {
+      const {
+        route,
+        params,
+        className,
+        activeClassName,
+        children,
+        ...newProps
+      } = props;
+      const nameOrUrl = route;
+
+      if (nameOrUrl) {
+        // eslint-disable-next-line react/no-this-in-sfc
+        Object.assign(newProps, this.findAndGetUrls(nameOrUrl, params).urls);
+      }
+
+      const isActiveRoute =
+        route === undefined
+          ? false
+          : // eslint-disable-next-line react/no-this-in-sfc
+            this.findAndGetUrls(route, params).urls.as === this.Router.asPath;
+
+      return (
+        // eslint-disable-next-line react/jsx-filename-extension
+        <Link {...newProps}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a
+            className={cx(className, {
+              [activeClassName]: isActiveRoute
+            })}
+          >
+            {children}
+          </a>
+        </Link>
+      );
+    };
+
+    return LinkRoutes;
+  }
+
+  getRouter(Router) {
+    const disallowTransition = routePath => {
+      const { route } = this.match(routePath);
+      const { isAuthenticated } = Router;
+
+      return (
+        route &&
+        ((route.onlyForUnauthenticated && isAuthenticated) ||
+          (route.onlyForAuthenticated && !isAuthenticated))
+      );
+    };
+    const wrap = method => (route, params, options) => {
+      const {
+        byName,
+        urls: { as, href }
+      } = this.findAndGetUrls(route, params);
+
+      if (disallowTransition(route)) {
+        return null;
+      }
+
+      return Router[method](href, as, byName ? options : params);
+    };
+
+    /* eslint-disable no-param-reassign */
+    Router.pushRoute = wrap('push');
+    Router.replaceRoute = wrap('replace');
+    Router.prefetchRoute = wrap('prefetch');
+
+    Router.toggleAuth = isAuthenticated => {
+      Router.isAuthenticated = _.isBoolean(isAuthenticated)
+        ? isAuthenticated
+        : !Router.isAuthenticated;
+    };
+    /* eslint-enable no-param-reassign */
+
+    return Router;
+  }
+}
 
 export default opts => new Routes(opts);
