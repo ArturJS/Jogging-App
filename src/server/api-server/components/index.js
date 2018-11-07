77ca8f436aaf5b8ac470fc34a4e01c1043593c2a
ArturJS <nizamutdin.art@gmail.com>
2018-11-07 07:10:53 +0400

    Added descriptive names for components files

--- src/server/api-server/components/index.js
+++ src/server/api-server/components/index.js
@@ -1,26 +1,26 @@
 import Long from 'graphql-type-long';
 import { makeExecutableSchema } from 'graphql-tools';
-import * as recordsQueries from './records/queries';
-import * as recordsMutations from './records/mutations';
-import * as reportsQueries from './reports/queries';
-import * as authQueries from './auth/queries';
-import * as authMutations from './auth/mutations';
+import * as recordsQueries from './records/records.queries';
+import * as recordsMutations from './records/records.mutations';
+import * as reportsQueries from './reports/reports.queries';
+import * as authQueries from './auth/auth.queries';
+import * as authMutations from './auth/auth.mutations';
 import typeDefs from './schema.graphql';
 
 const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers: {
-    Query: {
-      ...recordsQueries,
-      ...reportsQueries,
-      ...authQueries
-    },
-    Mutation: {
-      ...recordsMutations,
-      ...authMutations
-    },
-    Long
-  }
+    typeDefs,
+    resolvers: {
+        Query: {
+            ...recordsQueries,
+            ...reportsQueries,
+            ...authQueries
+        },
+        Mutation: {
+            ...recordsMutations,
+            ...authMutations
+        },
+        Long
+    }
 });
 
 export default schema;
