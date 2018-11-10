7719d4d19f6b77644659cb8aee9d74de35b899aa
ArturJS <nizamutdin.art@gmail.com>
2018-11-10 14:01:37 +0400

    Implemented 3 tier topology for 'auth' component

--- src/server/api-server/components/auth/auth.mutations.js
+++ src/server/api-server/components/auth/auth.mutations.js
@@ -1,53 +1,12 @@
-import db from '../../models';
-import { ErrorUserAlreadyExists, ErrorWrongCredentials } from './auth.errors';
-
-const emailUniquenessValidator = async signUpPayload => {
-    const { email } = signUpPayload;
-    const userWithSameEmail = await db.User.find({ where: { email } });
-
-    if (userWithSameEmail) {
-        throw new ErrorUserAlreadyExists();
-    }
-};
-
-const validateSignUp = async signUpPayload => {
-    await emailUniquenessValidator(signUpPayload);
-};
+import authBLL from './auth.bll';
 
 export const signIn = async (root, args, { auth }) => {
     const { email, password } = args;
+    const user = await authBLL.getUser({ email, password });
 
-    return new Promise((resolve, reject) => {
-        db.User.find({
-            where: {
-                email
-            }
-        }).then(user => {
-            if (!user) {
-                reject(new ErrorWrongCredentials());
+    auth.login(user);
 
-                return;
-            }
-
-            db.User.validPassword(
-                password,
-                user.password,
-                // eslint-disable-next-line no-shadow
-                (error, user) => {
-                    if (error) {
-                        reject(new ErrorWrongCredentials());
-
-                        return;
-                    }
-
-                    resolve(user);
-                },
-                user
-            );
-
-            auth.login(user);
-        });
-    });
+    return user;
 };
 
 export const signOut = async (root, args, { auth }) => {
@@ -57,17 +16,11 @@ export const signOut = async (root, args, { auth }) => {
 };
 
 export const signUp = async (root, args, { auth }) => {
-    await validateSignUp(args);
-
     const { firstName, lastName, email, password } = args;
-    const user = {
+    const user = await authBLL.createUser({
         firstName,
         lastName,
-        email
-    };
-
-    await db.User.create({
-        ...user,
+        email,
         password
     });
 
