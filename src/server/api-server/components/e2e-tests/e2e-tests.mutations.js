51a965f3bc79cc3febeca859d5b2c807722c9759
ArturJS <nizamutdin.art@gmail.com>
2018-12-09 20:42:23 +0400

    Added API for uploading fixtures (for e2e tests)

--- src/server/api-server/components/e2e-tests/e2e-tests.mutations.js
+++ src/server/api-server/components/e2e-tests/e2e-tests.mutations.js
@@ -1,14 +1,39 @@
 import baseResolver from '../base-resolver';
 import e2eTestsDAL from './e2e-tests.dal';
+import authBLL from '../auth/auth.bll';
+import recordsBLL from '../records/records.bll';
 
 export const createAll = baseResolver.createResolver(async (root, args) => {
-    const { allData } = args;
+    const { users } = args.allData;
 
-    console.log({
-        allData
-    });
+    // eslint-disable-next-line no-restricted-syntax
+    for (const user of users) {
+        // eslint-disable-next-line no-await-in-loop
+        const { id: userId } = await authBLL.createUser({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: user.password
+        });
+        const { records } = user;
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const record of records) {
+            // eslint-disable-next-line no-await-in-loop
+            await recordsBLL.createRecord({
+                date: record.date,
+                distance: record.distance,
+                time: record.time,
+                userId
+            });
+        }
+    }
+
+    return true;
 });
 
 export const dropAll = baseResolver.createResolver(async () => {
     await e2eTestsDAL.dropAll();
+
+    return true;
 });
